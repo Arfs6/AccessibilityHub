49fc59e0ae2c0735004305f218642915198b263d
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2023-10-29 14:19:35 +0100

    User session get saved on create account and login.

--- src/accessibilityHub/core/views.py
+++ src/accessibilityHub/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.contrib.auth import views as authViews
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 from .forms import CoreUserCreationForm
 
@@ -17,7 +18,7 @@ class CoreLoginView(authViews.LoginView):
     template_name = 'core/login.html'
 
 
-def createAccount(request):
+def createAccount(request: HttpRequest) -> HttpResponse:
     """View for creating accounts.
     """
     if request.method == "POST":
@@ -30,7 +31,8 @@ def createAccount(request):
             if data['lastName']:
                 newUser.last_name = data['lastName']
             newUser.save()
-            return HttpResponseRedirect("/")
+            login(request, newUser)
+            return HttpResponseRedirect(request.POST.get("next", "/"))
     else:
         _form = CoreUserCreationForm()
 
