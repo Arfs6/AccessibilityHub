2366980c9b641a4846c4c90ef1bf2075be1a7241
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2023-11-05 17:30:32 +0100

    created about page. populated home page. beautified whole site.

--- src/accessibilityHub/core/urls.py
+++ src/accessibilityHub/core/urls.py
@@ -3,25 +3,19 @@
 
 from django.urls import path
 from django.contrib.auth import views as authViews
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("accounts/login", views.CoreLoginView.as_view(), name="login"),
     path(
-        "accounts/login",
-        views.CoreLoginView.as_view(),
-        name="login"
+        "accounts/logout",
+        authViews.LogoutView.as_view(template_name="core/logout.html"),
+        name="logout",
     ),
-    path(
-        'accounts/logout',
-        authViews.LogoutView.as_view(template_name='core/logout.html'),
-        name='logout'
-    ),
-    path(
-        'accounts/signup',
-        views.createAccount,
-        name='createAccount'
-    )
+    path("accounts/signup", views.createAccount, name="createAccount"),
+    path("about", TemplateView.as_view(template_name="core/about.html"), name="about"),
 ]
