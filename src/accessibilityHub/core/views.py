1bc24c026100f5f9dc4ff2fb231781e7aa6c034a
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2024-02-24 19:14:41 +0100

    core: updated html of login page and formatted views.py file.

--- src/accessibilityHub/core/views.py
+++ src/accessibilityHub/core/views.py
@@ -8,53 +8,54 @@ from django.contrib.auth import login, logout
 
 from .forms import CoreUserCreationForm
 
+
 def home(request):
     """Renders the home page for Accessibility Hub."""
-    context = {
-            }
+    context = {}
     return render(request, "core/home.html", context)
 
 
 class CoreLoginView(authViews.LoginView):
     """Extends auth login view."""
-    template_name = 'core/login.html'
+
+    template_name = "core/login.html"
 
 
 def createAccount(request: HttpRequest) -> HttpResponse:
-    """View for creating accounts.
-    """
+    """View for creating accounts."""
     if request.method == "POST":
         _form = CoreUserCreationForm(request.POST)
         if _form.is_valid():
             newUser = _form.save()
             login(request, newUser)
-            next = request.POST.get('next')
+            next = request.POST.get("next")
             if next is None:
                 next = settings.LOGIN_REDIRECT_URL
             return HttpResponseRedirect(next)
         else:
-            next = request.POST.get('next')
+            next = request.POST.get("next")
     else:
         _form = CoreUserCreationForm()
-        next = request.GET.get('next')
+        next = request.GET.get("next")
 
     context = {
-            'form': _form,
-        'next': next,
-            }
+        "form": _form,
+        "next": next,
+    }
     return render(request, "core/create_account.html", context)
 
 
 def coreLogoutView(request):
-    """Logs the user out.
-    """
+    """Logs the user out."""
     if request.method == "DELETE":
         logout(request)
         response = HttpResponse(status=204)
-        next = request.GET.get('next')
+        next = request.GET.get("next")
         if not next:
             next = settings.LOGGIN_REDIRECT_URL
-        response['HX-Redirect'] = next
+        response["HX-Redirect"] = next
         return response
     else:
-        return render(request, "core/logout.html", context={'next': request.GET.get('next')})
+        return render(
+            request, "core/logout.html", context={"next": request.GET.get("next")}
+        )
