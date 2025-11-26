92abf2abb231b74168c50c0ebffa5a350ffb6a94
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2025-11-26 09:07:22 +0100

    Added tests for reviews urls. Unittest for the urlpatterns nad "integration" test for the urls, where status codes, templates and context were tested.

--- src/accessibilityHub/tests/test_reviews/test_urls.py
+++ src/accessibilityHub/tests/test_reviews/test_urls.py
@@ -1,10 +1,10 @@
 """Tests for url in the reviews app."""
 
-from django.test import TestCase, SimpleTestCase, Client
+from django.contrib.auth.models import User
+from django.test import Client, SimpleTestCase, TestCase
+from django.urls import reverse
+from reviews import forms, models, urls, views
 
-
-from reviews import urls
-from reviews import views
 
 class TestURLPatterns(SimpleTestCase):
     """Tests for the urlpatterns of the review apps."""
@@ -71,3 +71,142 @@ class TestURLPatterns(SimpleTestCase):
         self.assertIsNotNone(search)
         self.assertEqual(str(search.pattern), "search")
         self.assertEqual(search.callback, views.search)
+
+
+class TestURLS(TestCase):
+    """Test for urls
+    This tests are high level.
+    Test includes checks for templates, status codes and context (where applicable).
+    """
+
+    fixtures = ["User.json", "Tool.json", "Owner.json", "Review.json"]
+
+    def setup(self):
+        """Setup for each test."""
+        self.client = Client(ensure_csrf_check=True)
+
+    def test_index(self):
+        """Test for the index page."""
+        indexUrl = "reviews:index"
+        response = self.client.get(reverse(indexUrl))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/index.html")
+        self.assertQuerySetEqual(
+            response.context["tools"].order_by("pk"),
+            models.Tool.allVerified().order_by("pk"),
+        )
+
+    def test_owner(self):
+        """Test for the owner url."""
+        ownerUrl = "reviews:owner"
+        owner = models.Owner.allVerified().first()
+        self.assertIsNotNone(owner, "A verified `Owner` is required for testing.")
+        response = self.client.get(
+            reverse(ownerUrl, kwargs={"base36Id": owner.base36Id})
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/owner.html")
+        self.assertEqual(response.context["owner"], owner)
+        self.assertQuerySetEqual(
+            response.context["tools"], owner.tools.filter(verified=True).all()
+        )
+
+        owner = models.Owner.objects.filter(verified=False).first()
+        response = self.client.get(
+            reverse(ownerUrl, kwargs={"base36Id": owner.base36Id})
+        )
+        self.assertEqual(response.status_code, 404)
+
+        toolUrl = "reviews:tool"
+
+    def test_tool(self):
+        """Test for the tool url."""
+        toolUrl = "reviews:tool"
+        tool = models.Tool.allVerified().first()
+        response = self.client.get(
+            reverse(
+                toolUrl,
+                kwargs={"ownerBase36Id": tool.owner.base36Id, "toolSlug": tool.slug},
+            )
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/tool.html")
+        self.assertEqual(response.context["owner"], tool.owner)
+        self.assertEqual(response.context["tool"], tool)
+        self.assertQuerySetEqual(
+            response.context["reviews"].order_by("pk"),
+            tool.reviews.exclude(comment="").order_by("pk"),
+        )
+        self.assertIsNone(response.context["form"])  # user is anonymous
+        self.assertIsNone(response.context["formInstanceState"])
+
+        # Test for verified=False
+        tool = models.Tool.objects.filter(verified=False).first()
+        response = self.client.get(
+            reverse(
+                toolUrl,
+                kwargs={"ownerBase36Id": tool.owner.base36Id, "toolSlug": tool.slug},
+            )
+        )
+        self.assertEqual(response.status_code, 404)
+
+        # Test with logged in user.
+        tool = models.Tool.allVerified().first()
+        user = User.objects.exclude(
+            reviews__tool=tool
+        ).first()  # user hasn't reviewed tool
+        self.assertRaises(models.Review.DoesNotExist, tool.reviews.get, user=user)
+        self.client.force_login(user)
+        response = self.client.get(
+            reverse(
+                toolUrl,
+                kwargs={"ownerBase36Id": tool.owner.base36Id, "toolSlug": tool.slug},
+            )
+        )
+        self.assertIsNotNone(response.context["form"])
+        _form = forms.ReviewForm()
+        self.assertDictEqual(_form.initial, response.context["form"].initial)
+
+        # Let's create a review.
+        response = self.client.post(
+            reverse(
+                toolUrl,
+                kwargs={"ownerBase36Id": tool.owner.base36Id, "toolSlug": tool.slug},
+            ),
+            data={"rating": 3},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/tool.html")
+        _form = forms.ReviewForm(instance=user.reviews.get(tool=tool))
+        self.assertEqual(response.context["form"].instance, _form.instance)
+
+        # Test for put.
+        response = self.client.put(
+            reverse(
+                toolUrl,
+                kwargs={"ownerBase36Id": tool.owner.base36Id, "toolSlug": tool.slug},
+            ),
+            data={"comment": "This is a long text.\nIt could be multiple lines."},
+        )
+        self.assertEqual(response.status_code, 200)
+        _form = forms.ReviewForm(instance=tool.reviews.get(user=user))
+
+    def test_new(self):
+        """Test for the new url."""
+        newUrl = "reviews:newTool"
+        response = self.client.get(reverse(newUrl))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/new_tool.html")
+        self.assertEqual(response.context["title"], "Request New Tool")
+
+    def test_search(self):
+        """Test for the search url."""
+        searchUrl = "reviews:search"
+        tool = models.Tool.allVerified().first()
+        response = self.client.get(reverse(searchUrl), data={"searchTerm": tool.name[1:4]})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "reviews/search.html")
+        self.assertIn(tool,response.context["tools"] )
+
+        response = self.client.get(reverse(searchUrl))
+        self.assertEqual(response.status_code, 400)
