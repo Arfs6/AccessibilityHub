4049c2fa898362a28431db20886c2bf829f9a323
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2025-12-07 13:35:58 +0100

    Fixed linter errors in fabfile.py Decided to selectively ignore the  error as the script moves the file immediately after ting it. I can't se the difference between using  and  apart from cluttering the home directory.

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,53 +1,56 @@
-# -*- coding: utf-8 -*-
 """Automate seting up servers and deploying code."""
-from fabric import task, Connection
-import logging
-import json
-from typing import Generator, Optional
-import sys
 
+import json
+import logging
+import sys
+from collections.abc import Generator
+
+from fabric import Connection, task
 
 log = logging.getLogger()
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 consoleHandler.setFormatter(formatter)
 log.addHandler(consoleHandler)
 log.setLevel(logging.INFO)
-log.debug("Hello Fabric world")
 
 try:
     with open('servers_config.json') as fileObj:
         config = json.load(fileObj)
 except FileNotFoundError:
-    log.error("Could not find the servers_config.json file. Make sure you have it and it is in the same directory with your fab file.")
+    log.exception(
+        'Could not find the servers_config.json file. Make sure you have it and it is in the same directory with your fab file.'
+    )
     sys.exit()
-except json.JSONDecodeError as mess:
-    log.error(f"You have a syntax error in your server_config.json file: {mess}")
+except json.JSONDecodeError:
+    log.exception('You have a syntax error in your server_config.json file ')
     sys.exit()
 
 
 def allServers() -> Generator:
-    """Yields Connection objections for servers."""
+    """Yields Connection objects for servers."""
     for server in config['servers']:
         if server.get('type') == 'lb':
             continue
-        con = Connection(host=server['host'], user=server['user'], connect_kwargs={"key_filename": config['private_key']})
+        con = Connection(
+            host=server['host'], user=server['user'], connect_kwargs={'key_filename': config['private_key']}
+        )
         yield con
 
 
 def _installPython(c):
     """Installs python and all it's dependencies."""
-    c.run("sudo apt-get update")
-    c.run("sudo add-apt-repository ppa:deadsnakes/ppa")
-    c.run("sudo apt-get install python3.11 -y")
-    c.run("curl https://bootstrap.pypa.io/get-pip.py | sudo python3.11")
-    extras = ["python3.11-dev", "python3.11-distutils ", "python3.11-venv"]
-    c.run("sudo apt-get install -y " + ' '.join(extras))
+    c.run('sudo apt-get update')
+    c.run('sudo add-apt-repository ppa:deadsnakes/ppa')
+    c.run('sudo apt-get install python3.11 -y')
+    c.run('curl https://bootstrap.pypa.io/get-pip.py | sudo python3.11')
+    extras = ['python3.11-dev', 'python3.11-distutils ', 'python3.11-venv']
+    c.run('sudo apt-get install -y ' + ' '.join(extras))
 
 
 @task
-def installPython(c):
+def installPython(c):  # noqa: ARG001
     """Iterates through all servers and installs python."""
     for con in allServers():
         _installPython(con)
@@ -56,13 +59,13 @@ def installPython(c):
 def _setupPythonEnv(c):
     """Clones the repo and creates a virtual environment"""
     with c.cd('~'):
-        c.run("git clone https://github.com/arfs6/AccessibilityHub")
+        c.run('git clone https://github.com/arfs6/AccessibilityHub')
     with c.cd('~/AccessibilityHub'):
-        c.run("python3.11 -m venv env")
+        c.run('python3.11 -m venv env')
 
 
 @task
-def setupPythonEnv(c):
+def setupPythonEnv(c):  # noqa: ARG001
     """setups python environment in both servers."""
     for con in allServers():
         _setupPythonEnv(con)
@@ -70,19 +73,19 @@ def setupPythonEnv(c):
 
 def _setupGunicorn(c: Connection):
     """Sets up the gunicorn socket and service.d script."""
-    c.put('systemd/gunicorn.socket', remote='/tmp')
-    c.put('systemd/gunicorn.service', remote='/tmp')
+    c.put('systemd/gunicorn.socket', remote='/tmp')  # noqa: S108
+    c.put('systemd/gunicorn.service', remote='/tmp')  # noqa: S108
     with c.cd('/etc/systemd/system/'):
-            c.run("sudo mv /tmp/gunicorn.socket ./")
-            c.run("sudo mv /tmp/gunicorn.service ./")
-    c.run("sudo systemctl start gunicorn.socket")
-    c.run("sudo systemctl enable gunicorn.socket")
-    c.run("sudo systemctl daemon-reload")
-    c.run("sudo systemctl restart gunicorn.socket")
+        c.run('sudo mv /tmp/gunicorn.socket ./')
+        c.run('sudo mv /tmp/gunicorn.service ./')
+    c.run('sudo systemctl start gunicorn.socket')
+    c.run('sudo systemctl enable gunicorn.socket')
+    c.run('sudo systemctl daemon-reload')
+    c.run('sudo systemctl restart gunicorn.socket')
 
 
 @task
-def setupGunicorn(c):
+def setupGunicorn(c):  # noqa: ARG001
     """Setup gunicorn in both servers."""
     for con in allServers():
         _setupGunicorn(con)
@@ -90,61 +93,63 @@ def setupGunicorn(c):
 
 def _setupNginx(c: Connection):
     """Setup nginx configuration and service."""
-    c.run("sudo apt-get update")
-    c.run("sudo apt-get install nginx -y")
-    c.put("nginx/accessibilityhub", remote='/tmp')
-    with c.cd("/etc/nginx/"):
-        c.run("sudo mv /tmp/accessibilityhub sites-available")
-        c.run("sudo ln -fs /etc/nginx/sites-available/accessibilityhub /etc/nginx/sites-enabled/default")
-        if c.run("sudo nginx -t").return_code != 0:
-            log.error("Nginx configuration failed\nFile at deploy/nginx/accessibilityhub.")
+    c.run('sudo apt-get update')
+    c.run('sudo apt-get install nginx -y')
+    c.put('nginx/accessibilityhub', remote='/tmp')  # noqa: S108
+    with c.cd('/etc/nginx/'):
+        c.run('sudo mv /tmp/accessibilityhub sites-available')
+        c.run('sudo ln -fs /etc/nginx/sites-available/accessibilityhub /etc/nginx/sites-enabled/default')
+        if c.run('sudo nginx -t').return_code != 0:
+            log.error('Nginx configuration failed\nFile at deploy/nginx/accessibilityhub.')
             sys.exit()
-    c.run("sudo service nginx restart")
+    c.run('sudo service nginx restart')
 
 
 @task
-def setupNginx(c):
+def setupNginx(c):  # noqa: ARG001
     """Setup nginx on both servers."""
     for idx, con in enumerate(allServers()):
-        log.info(f"Setting up nginx in server #{idx}")
+        log.info('Setting up nginx in server #%s', idx)
         _setupNginx(con)
 
 
 @task
-def setupUfw(c):
+def setupUfw(c):  # noqa: ARG001
     """Setup ufw."""
     for con in allServers():
-        con.run("sudo ufw allow ssh")
-        con.run("sudo ufw allow 3306")
+        con.run('sudo ufw allow ssh')
+        con.run('sudo ufw allow 3306')
         con.run("sudo ufw allow 'Nginx HTTP'")
-        con.run("sudo ufw enable")
-        con.run("sudo ufw reload")
+        con.run('sudo ufw enable')
+        con.run('sudo ufw reload')
 
 
 @task
-def setupLoadBalancer(c):
+def setupLoadBalancer(c):  # noqa: ARG001
     """Configures haproxy on load balancer server and enable firewall."""
-    lbServer: Optional[dict] = None
+    lbServer: dict | None = None
     for server in config['servers']:
         if server.get('type') == 'lb':
             lbServer = server
             break
     if lbServer is None:
-        log.error("No load balancer in server config file.")
+        log.error('No load balancer in server config file.')
         return
-    con = Connection(host=lbServer['host'], user=lbServer['user'], connect_kwargs={"key_filename": config['private_key']})
-    con.sudo("apt-get update")
-    con.sudo("apt-get install haproxy -y")
+    con = Connection(
+        host=lbServer['host'], user=lbServer['user'], connect_kwargs={'key_filename': config['private_key']}
+    )
+    con.sudo('apt-get update')
+    con.sudo('apt-get install haproxy -y')
     # enable init script for haproxy.
     # Make sure running the command twice has no effect.
-    con.sudo("mv /etc/haproxy/haproxy.cfg{,.old}")
-    con.put("haproxy/haproxy.cfg", remote='/tmp')
-    con.sudo("mv /tmp/haproxy.cfg /etc/haproxy/haproxy.cfg")
-    con.sudo("service haproxy restart")
+    con.sudo('mv /etc/haproxy/haproxy.cfg{,.old}')
+    con.put('haproxy/haproxy.cfg', remote='/tmp')  # noqa: S108
+    con.sudo('mv /tmp/haproxy.cfg /etc/haproxy/haproxy.cfg')
+    con.sudo('service haproxy restart')
 
 
 @task
-def setupServers(c):
+def setupServers(c):  # noqa: ARG001
     """Install everything we need to run our django app."""
     for con in allServers():
         _installPython(con)
@@ -155,22 +160,22 @@ def setupServers(c):
 
 
 @task
-def updatePipDependencies(c):
+def updatePipDependencies(c):  # noqa: ARG001
     """Installs new pip dependencies on server."""
     for con in allServers():
-        with con.cd("~/AccessibilityHub"):
-            con.run("git pull")
-            con.run("env/bin/python -m pip install -r requirements.txt")
+        with con.cd('~/AccessibilityHub'):
+            con.run('git pull')
+            con.run('env/bin/python -m pip install -r requirements.txt')
 
 
 @task
-def deploy(c):
+def deploy(c):  # noqa: ARG001
     """Deploys a new version of Accessibility Hub."""
     for con in allServers():
-        with con.cd("~/AccessibilityHub"):
-            con.run("git pull")
-        with con.cd("~/AccessibilityHub/src/accessibilityHub"):
-            con.run("../../env/bin/python manage.py makemigrations")
-            con.sudo("../../env/bin/python manage.py migrate")
-        con.run("sudo systemctl restart gunicorn.socket")
-        con.sudo("systemctl restart gunicorn")
+        with con.cd('~/AccessibilityHub'):
+            con.run('git pull')
+        with con.cd('~/AccessibilityHub/src/accessibilityHub'):
+            con.run('../../env/bin/python manage.py makemigrations')
+            con.sudo('../../env/bin/python manage.py migrate')
+        con.run('sudo systemctl restart gunicorn.socket')
+        con.sudo('systemctl restart gunicorn')
